87109f30d343c110084b752c425e67f2873a1fef
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-20 11:15:17 -0800

    Actually use the node item. Bind publicly if master.

--- recipes/_replication.rb
+++ recipes/_replication.rb
@@ -1,7 +1,9 @@
 if(node['redis']['replication']['redis_replication_role'] == 'slave')
-  master = search(:node, 'redis_replication_role:master')
+  master = search(:node, 'redis_replication_role:master').first
   if(master)
     node.set['redis']['config']['slaveof'] = "#{master[:ipaddress]} #{master['redis']['config']['listen_port']}"
   end
+else
+  node[:redis][:config][:bind] = node.ipaddress
 end
 
