bde5376f0dcadcc3362d2630d91073bbc3046c80
Chris Roberts <chrisroberts.code@gmail.com>
2012-11-20 11:14:46 -0800

    Fix up some config building issues

--- libraries/chef_redis.rb
+++ libraries/chef_redis.rb
@@ -1,19 +1,21 @@
 module ChefRedis
-  def template_format(key, v)
-    k = format_key(k)
-    case v
-    when TrueClass
-      "#{k} on"
-    when FalseClass
-      "#{k} off"
-    when Array
-      v.map{|value| template_format(k, value) }.join("\n")
-    else
-      "#{k} #{v}"
+  class << self
+    def template_format(key, v)
+      k = format_key(key)
+      case v
+      when TrueClass
+        "#{k} yes"
+      when FalseClass
+        "#{k} no"
+      when Array
+        v.map{|value| template_format(k, value) }.join("\n")
+      else
+        "#{k} #{v}"
+      end
     end
-  end
 
-  def format_key(k)
-    k.to_s.gsub('_', '-')
+    def format_key(k)
+      k.to_s.gsub('_', '-')
+    end
   end
 end
