b58b27cd536a5ed41e2e4304a2ebb9cad89d354d
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-28 16:37:47 -0800

    Clean up sentinel configurations

--- recipes/sentinel.rb
+++ recipes/sentinel.rb
@@ -1,28 +1,21 @@
+if(node['redis']['replication']['tunnel']['enabled'])
+  raise 'Sentinel is not currently supported with tunneled connections'
+end
+
 include_recipe 'redis::replication'
 
 basics = {
-  :down_after_milliseconds => node[:redis][:sentinel][:down_after],
-  :failover_timeout => node[:redis][:sentinel][:failover_timeout],
-  :can_failover => node[:redis][:sentinel][:can_failover],
-  :parallel_syncs => node[:redis][:sentinel][:parallel_syncs]
+  'down_after_milliseconds' => node['redis']['sentinel']['down_after'],
+  'failover_timeout' => node['redis']['sentinel']['failover_timeout'],
+  'can_failover' => node['redis']['sentinel']['can_failover'],
+  'parallel_syncs' => node['redis']['sentinel']['parallel_syncs']
 }
 
-if(node['redis']['replication']['redis_replication_role'] == 'slave')
-  master = search(:node, 'redis_replication_role:master').first
-  if(master)
-    node.default[:redis][:sentinel][:mon_config][master.name] = {
-      :monitor => "#{master[:ipaddress]} #{master[:redis][:config][:port]} #{node[:redis][:sentinel][:quorum]}"
-    }.merge(basics)
-  else
-    Chef::Log.warn 'Failed to locate redis master instance'
-  end
-else
-  node.default[:redis][:sentinel][:mon_config][node.name] = {
-    :monitor => "#{node[:ipaddress]} #{node[:redis][:config][:port]} #{node[:redis][:sentinel][:quorum]}"
-  }.merge(basics)
-end
+node.default['redis']['sentinel']['mon_config'][node.name] = {
+  :monitor => "#{node['redis']['config']['bind']} #{node['redis']['config']['port']} #{node['redis']['sentinel']['quorum']}"
+}.merge(basics)
 
-template File.join(node[:redis][:conf_dir], 'sentinel.conf') do
+template File.join(node['redis']['conf_dir'], 'sentinel.conf') do
   source 'sentinel.conf.erb'
   mode 0644
   notifies :restart, 'service[redis-sentinel]'
