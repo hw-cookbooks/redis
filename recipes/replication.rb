262fa9d0193b93a1e1021757a0e381c5b0c3c2ad
Chris Roberts <chrisroberts.code@gmail.com>
2012-12-28 16:36:41 -0800

    Update stunnel configuration. Discover master via discovery and restrict to common env

--- recipes/replication.rb
+++ recipes/replication.rb
@@ -1,5 +1,14 @@
+unless(node['redis']['replication']['tunnel']['enabled'])
+  node.default['redis']['config']['bind'] = node[:ipaddress]
+end
+
 if(node['redis']['replication']['redis_replication_role'] == 'slave')
-  master = search(:node, 'redis_replication_role:master').first
+  master = discovery_search(
+    'redis_replication_role:master',
+    :environment_aware => true,
+    :raw_search => true,
+    :minimum_response_time_sec => nil
+  )
 
   if(master)
     if(node['redis']['replication']['tunnel']['enabled'])
@@ -7,7 +16,7 @@ if(node['redis']['replication']['redis_replication_role'] == 'slave')
 
       stunnel_connection 'redis' do
         connect "#{master['ipaddress']}:#{master['redis']['replication']['tunnel']['accept_port']}"
-        accept node['redis']['replication']['tunnel']['accept_port']
+        accept "127.0.0.1:#{node['redis']['replication']['tunnel']['accept_port']}"
         notifies :restart, 'service[stunnel]'
       end
 
@@ -20,17 +29,17 @@ else
   if(node['redis']['replication']['tunnel']['enabled'])
     include_recipe 'stunnel::server'
 
+    node.normal['redis']['config']['bind'] = '127.0.0.1'
+
     stunnel_connection 'redis' do
       accept node['redis']['replication']['tunnel']['accept_port']
-      connect node['redis']['config']['port']
+      connect "#{node['redis']['config']['bind']}:#{node['redis']['config']['port']}"
       notifies :restart, 'service[stunnel]'
     end
 
     service "redis" do
       notifies :restart, 'service[stunnel]'
     end
-
-    node.normal['redis']['config']['bind'] = '127.0.0.1'
   else
     node.normal['redis']['config']['bind'] = node[:ipaddress]
   end
